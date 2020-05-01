3ff957be3c7d78c29ccce558cb6e0d2e2f8b6fa6
Heber <hebergonza@gmail.com>
2020-04-30 21:41:31 -0400

    initial commit

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+// const Hobbits = require("../hobbits/hobbitsModel.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+// we need to add routers
+
+module.exports = server;
